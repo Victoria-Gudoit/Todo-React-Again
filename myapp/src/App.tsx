30da370ea0c56cef0bcb36ac735d7f0a90b22fbd
Victoria-Gudoit <vgudojt@mail.ru>
2023-10-09 14:48:34 +0300

    feat: add html code in App.tsk

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const tasks = [
+    { id: 1, title: "замедлиться", isDone: false },
+    { id: 2, title: "жить", isDone: true },
+  ];
+
+  return (
+    <div className="App">
+      <div>
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <div>
+            <input type="checkbox" checked={task.isDone} />
+            <li>{task.title}</li>
+          </div>
+        ))}
+      </ul>
+      <div>
+        <button>Всё</button>
+        <button>Сделать</button>
+        <button>Сделано</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
